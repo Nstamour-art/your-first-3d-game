cb0adce55a82aa50feb512019fe0375157348328
Nicolas St-Amour <nstamour.dev@outlook.com>
2026-03-15 19:34:22 -0400

    First Commit

---
+++ 3d_squash_the_creeps_starter/scripts/mob.gd
@@ -0,0 +1,19 @@
+extends CharacterBody3D
+
+@export var min_speed = 10
+@export var max_speed = 18
+
+func _physics_process(delta: float) -> void:
+    move_and_slide()
+    
+func initialize(start_position, player_position) -> void:
+    look_at_from_position(start_position, player_position, Vector3.UP)
+    rotate_y(randf_range(-PI / 4, PI / 4))
+    print(min_speed)
+    print(max_speed)
+    var random_speed = randi_range(min_speed, max_speed)
+    velocity = velocity.rotated(Vector3.UP, rotation.y)
+    
+
+func _on_screen_exited() -> void:
+    queue_free()
