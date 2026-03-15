3b102d08f880833fa4897c749e61d735c3d361ab
Nstamour-art <nicolassta@gmail.com>
2026-03-15 19:54:52 -0400

    Fix for speed vars
    Fixed issue with int initialization for speed vars

--- 3d_squash_the_creeps_starter/scripts/mob.gd
+++ 3d_squash_the_creeps_starter/scripts/mob.gd
@@ -1,19 +1,19 @@
 extends CharacterBody3D
 
-@export var min_speed = 10
-@export var max_speed = 18
+# Minimum speed of the mob in meters per second.
+@export var min_speed: int = 10
+# Maximum speed of the mob in meters per second.
+@export var max_speed: int = 18
 
-func _physics_process(delta: float) -> void:
-    move_and_slide()
-    
-func initialize(start_position, player_position) -> void:
-    look_at_from_position(start_position, player_position, Vector3.UP)
-    rotate_y(randf_range(-PI / 4, PI / 4))
-    print(min_speed)
-    print(max_speed)
-    var random_speed = randi_range(min_speed, max_speed)
-    velocity = velocity.rotated(Vector3.UP, rotation.y)
-    
+func _physics_process(_delta):
+	move_and_slide()
 
-func _on_screen_exited() -> void:
-    queue_free()
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(randf_range(-PI / 4, PI / 4))
+	var random_speed = randi_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+func _on_visible_on_screen_notifier_3d_screen_exited():
+	queue_free()
